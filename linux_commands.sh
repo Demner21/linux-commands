ba652aa6f9f51b0ac52066c29537ed3e4c08690f
Demneru <18225600+Demner21@users.noreply.github.com>
2021-03-03 16:15:10 -0500

    Create linux_commands.sh
    subidad de fuentes para creacion de sh con comandos linux de uso diario

---
+++ linux_commands.sh
@@ -0,0 +1,13 @@
+
+#GREP
+#usando grep para busqueda con regex de hora por ejemplo
+#notese que se usa \ para separar el uso del caracter [
+grep -rnw '\[02-03-2021 17:41:[[:digit:]]\{2\}\.[[:digit:]]\{1,3\}\] (Handler.java:55)*' * | head -10
+
+#buscara por ejemplo
+#SERVER: osbServer_01 [DEBUG] [02-03-2021 17:41:25.260] (Handler.java:55) - [ID: 202103020541242605] 
+#SERVER: osbServer_01 [DEBUG] [02-03-2021 17:41:32.867] (Handler.java:55) - [ID: 6ec0d543-695e-4652-b447-8d01c4e96590] 
+#SERVER: osbServer_01 [DEBUG] [02-03-2021 17:41:32.868] (Handler.java:55) - [ID: 6ec0d543-695e-4652-b447-8d01c4e96590] 
+#SERVER: osbServer_01 [DEBUG] [02-03-2021 17:41:46.388] (Handler.java:55) - [ID: 6d994c6e-b9bd-43db-8389-955301b8f868] 
+#SERVER: osbServer_01 [DEBUG] [02-03-2021 17:41:46.388] (Handler.java:55) - [ID: 6d994c6e-b9bd-43db-8389-955301b8f868] 
+
