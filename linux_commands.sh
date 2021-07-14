fe27b182cb5b71e1fdad93a442e442484f4fe197
Demneru <18225600+Demner21@users.noreply.github.com>
2021-07-14 11:14:39 -0500

    Update linux_commands.sh
    Listar los directorios y archivos en la carpeta actual

--- linux_commands.sh
+++ linux_commands.sh
@@ -38,3 +38,6 @@ docker ps -a | grep -v Up | awk '{ print $1; }' | xargs docker rm
 docker images | grep none | awk '{ print $3; }' | xargs docker rmi
 
 
+#Listar los directorios y archivos en la carpeta actual
+find . -type f -follow -print
+
